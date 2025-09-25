800b58b1a14703b10b28f040df8a799b62134a13
hyunwoo3671 <k67810621@gmail.com>
2025-09-25 17:21:52 +0900

    k
    k

--- Scripts/activate.fish
+++ Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'D:\django\study\django'
+set -gx VIRTUAL_ENV 'D:\django\study\django\django'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Post
 
+def index(request):
+    Posts = Post.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts': Posts,
+        }
+    )
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post': post,
+        }
+    )
 # Create your views here.
